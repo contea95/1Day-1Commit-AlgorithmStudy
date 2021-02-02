cadeb8f9a54a3cc824a5571198eb380dc60a2fc7
Sang Hyeok Han <contea95@naver.com>
2021-02-02 23:52:46 +0900

    5장 문제 일부

---
+++ BOJ/Python/Practice/5장/05-1.py
@@ -0,0 +1,21 @@
+class Calculator:
+    def __init__(self):
+        self.value = 0
+
+    def add(self, val):
+        self.value += val
+
+
+class UpgradeCalculator(Calculator):
+    def __init__(self):
+        self.value = 0
+
+    def minus(self, value):
+        self.value -= value
+
+
+cal = UpgradeCalculator()
+cal.add(10)
+cal.minus(7)
+
+print(cal.value)
