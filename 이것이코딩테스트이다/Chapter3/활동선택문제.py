64c491e0dea90f52ff0b95950331f38f1ee6c9e8
Sang Hyeok Han <contea95@naver.com>
2021-04-28 10:24:53 +0900

    활동선택문제

---
+++ 이것이코딩테스트이다/Chapter3/활동선택문제.py
@@ -0,0 +1,18 @@
+n = int(input())
+time_list = []
+last_idx = 0
+cnt = 0
+for i in range(n):
+    time_list.append(list(map(int, input().split())))
+
+time_list.sort(key=lambda x: (x[1], x[0])) # Fi 기준 정렬 후 Si 기준 정렬
+
+for i in range(len(time_list)):
+    if time_list[i][0] >= last_idx:
+        last_idx = time_list[i][1]
+        cnt += 1
+        continue
+    else:
+        continue
+
+print(cnt)
